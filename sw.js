4f20c6967453d9ce940793be15f7b8362396f575
Dara Khoyi <dara@brokerdara.com>
2026-06-18 19:40:31 +0000

    Updates

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 //   - New versions of the SW skip-waiting + claim clients immediately so a
 //     deploy is picked up on next page load without an extra refresh
 
-const VERSION = 'prismos-v17-20260618-amrfix'
+const VERSION = 'prismos-v18-20260618-amrworker'
 const APP_SHELL = [
   '/',
   '/index.html',
