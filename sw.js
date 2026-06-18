c9a7826b5551929f1abd18c2fccbc22a9af72a84
Dara Khoyi <dara@brokerdara.com>
2026-06-18 02:55:54 +0000

    Updates

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 //   - New versions of the SW skip-waiting + claim clients immediately so a
 //     deploy is picked up on next page load without an extra refresh
 
-const VERSION = 'prismos-v24-20260617-tasks-icons'
+const VERSION = 'prismos-v25-20260617-calendar-icons'
 const APP_SHELL = [
   '/',
   '/index.html',
