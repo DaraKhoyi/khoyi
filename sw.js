ee6ce9df8e3ee7bdd806ec0882bfcecea4d81833
Dara Khoyi <dara@brokerdara.com>
2026-06-21 13:39:10 +0000

    Updates

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 //   - New versions of the SW skip-waiting + claim clients immediately so a
 //     deploy is picked up on next page load without an extra refresh
 
-const VERSION = 'prismos-v51-20260620-vite'
+const VERSION = 'prismos-v52-20260620-vendor-split'
 const APP_SHELL = [
   '/',
   '/index.html',
