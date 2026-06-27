4a5b40080a14c13ea7671fb76cec988fc6fb6af9
Dara Khoyi <dara@brokerdara.com>
2026-06-27 13:02:08 +0000

    Updates

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 //   - New versions of the SW skip-waiting + claim clients immediately so a
 //     deploy is picked up on next page load without an extra refresh
 
-const VERSION = 'prismos-v123-20260627-leadgen-pipeline-kpi'
+const VERSION = 'prismos-v124-20260627-pipeline-dashboard-kpi'
 const APP_SHELL = [
   '/',
   '/index.html',
