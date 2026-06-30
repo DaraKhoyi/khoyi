19db89f4270d7e194ad5a47f82725155a541d58c
Dara Khoyi <dara@brokerdara.com>
2026-06-30 22:47:59 +0000

    Updates

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 //     activates it on demand. This guarantees deploys are picked up promptly
 //     (even on a resumed/backgrounded PWA) without yanking the bundle mid-task.
 
-const VERSION = 'prismos-v163-20260630-update-no-hang'
+const VERSION = 'prismos-v164-20260630-nba-mark-replied'
 const APP_SHELL = [
   '/',
   '/index.html',
