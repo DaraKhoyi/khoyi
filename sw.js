04ea9cb5dafb3d04ebb9308f1728762b5ced23ce
Dara Khoyi <dara@brokerdara.com>
2026-07-01 13:56:38 +0000

    Updates

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 //     activates it on demand. This guarantees deploys are picked up promptly
 //     (even on a resumed/backgrounded PWA) without yanking the bundle mid-task.
 
-const VERSION = 'prismos-v170-20260701-nba-open-real-thread'
+const VERSION = 'prismos-v171-20260701-nba-open-acct-aware'
 const APP_SHELL = [
   '/',
   '/index.html',
