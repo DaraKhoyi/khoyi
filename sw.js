117d80f86a6cdc6aaefa45fb82e351409e1c07a8
Dara Khoyi <dara@brokerdara.com>
2026-07-01 12:54:19 +0000

    Updates

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 //     activates it on demand. This guarantees deploys are picked up promptly
 //     (even on a resumed/backgrounded PWA) without yanking the bundle mid-task.
 
-const VERSION = 'prismos-v169-20260701-tasks-multiselect'
+const VERSION = 'prismos-v170-20260701-nba-open-real-thread'
 const APP_SHELL = [
   '/',
   '/index.html',
