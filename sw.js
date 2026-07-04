1137b1f826fc53c49963fc51f0f3461274e448f0
Dara Khoyi <dara@brokerdara.com>
2026-07-04 17:51:33 +0000

    Updates

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 //     activates it on demand. This guarantees deploys are picked up promptly
 //     (even on a resumed/backgrounded PWA) without yanking the bundle mid-task.
 
-const VERSION = 'prismos-v208-20260704-daypicker-botharrows'
+const VERSION = 'prismos-v209-20260704-weekof-symmetric'
 const APP_SHELL = [
   '/',
   '/index.html',
