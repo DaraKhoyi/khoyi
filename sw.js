d8a888c4a5cf9bcb4308963c13ebe6e8ded1a29d
Dara Khoyi <dara@brokerdara.com>
2026-07-04 12:13:17 +0000

    Updates

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 //     activates it on demand. This guarantees deploys are picked up promptly
 //     (even on a resumed/backgrounded PWA) without yanking the bundle mid-task.
 
-const VERSION = 'prismos-v197-20260704-review-delete-call-play'
+const VERSION = 'prismos-v198-20260704-autoschedule-toggle'
 const APP_SHELL = [
   '/',
   '/index.html',
