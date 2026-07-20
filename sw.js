74e61c45db9da46e56cdb59a1c9ccc3561c861e9
Dara Khoyi <dara@brokerdara.com>
2026-07-20 01:47:59 +0000

    Deploy b025b2f2 (fork slower)

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 //     activates it on demand. This guarantees deploys are picked up promptly
 //     (even on a resumed/backgrounded PWA) without yanking the bundle mid-task.
 
-const VERSION = 'prismos-f77894f4'
+const VERSION = 'prismos-b025b2f2'
 const APP_SHELL = [
   '/',
   '/index.html',
