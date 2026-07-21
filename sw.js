25c530e59844d7a6ccf6c1ed9fc8980c2d98d164
Dara Khoyi <dara@brokerdara.com>
2026-07-21 18:36:12 +0000

    Deploy fb3d8c22 (10x dialer)

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 //     activates it on demand. This guarantees deploys are picked up promptly
 //     (even on a resumed/backgrounded PWA) without yanking the bundle mid-task.
 
-const VERSION = 'prismos-5ab4fbe'
+const VERSION = 'prismos-fb3d8c22'
 const APP_SHELL = [
   '/',
   '/index.html',
