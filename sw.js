d428f248134a16dca7c5f774b56126f8e07412e7
Dara Khoyi <dara@brokerdara.com>
2026-07-21 12:42:48 +0000

    Deploy 06ed1333 (Not a Lead option)

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 //     activates it on demand. This guarantees deploys are picked up promptly
 //     (even on a resumed/backgrounded PWA) without yanking the bundle mid-task.
 
-const VERSION = 'prismos-2c6bd80'
+const VERSION = 'prismos-06ed1333'
 const APP_SHELL = [
   '/',
   '/index.html',
