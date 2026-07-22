058e4865400f66a9b92aa7e509d78f6878923761
Dara Khoyi <dara@brokerdara.com>
2026-07-22 02:37:07 +0000

    Deploy c8fee14c (focus fix + tip width)

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 //     activates it on demand. This guarantees deploys are picked up promptly
 //     (even on a resumed/backgrounded PWA) without yanking the bundle mid-task.
 
-const VERSION = 'prismos-dc7d13a'
+const VERSION = 'prismos-c8fee14c'
 const APP_SHELL = [
   '/',
   '/index.html',
