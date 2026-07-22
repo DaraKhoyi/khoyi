0a5bc714d1c53be38fe3e764dfe726f3de5e61f2
Dara Khoyi <dara@brokerdara.com>
2026-07-22 14:08:51 +0000

    sw 07275ae7

--- public/sw.js
+++ public/sw.js
@@ -12,7 +12,7 @@
 //     activates it on demand. This guarantees deploys are picked up promptly
 //     (even on a resumed/backgrounded PWA) without yanking the bundle mid-task.
 
-const VERSION = 'prismos-5710d58e'
+const VERSION = 'prismos-07275ae7'
 const APP_SHELL = [
   '/',
   '/index.html',
