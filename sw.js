b791bf98cb969ce950ed2430ab7b53fd6701d77a
Dara Khoyi <dara@brokerdara.com>
2026-07-22 03:33:55 +0000

    Deploy 487f0766 (retire dashboard)

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 //     activates it on demand. This guarantees deploys are picked up promptly
 //     (even on a resumed/backgrounded PWA) without yanking the bundle mid-task.
 
-const VERSION = 'prismos-ed88e2f'
+const VERSION = 'prismos-487f0766'
 const APP_SHELL = [
   '/',
   '/index.html',
