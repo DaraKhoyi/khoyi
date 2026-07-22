37838e008f1db541068867271fea47fbb7b16a50
Dara Khoyi <dara@brokerdara.com>
2026-07-22 04:13:40 +0000

    Deploy 72d01ae5 (full task list default)

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 //     activates it on demand. This guarantees deploys are picked up promptly
 //     (even on a resumed/backgrounded PWA) without yanking the bundle mid-task.
 
-const VERSION = 'prismos-6f7b53d'
+const VERSION = 'prismos-72d01ae5'
 const APP_SHELL = [
   '/',
   '/index.html',
