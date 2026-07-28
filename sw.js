8dd54ff29c76c28335feac0e9e5ac6760cc37b64
DaraKhoyi <DaraKhoyi@users.noreply.github.com>
2026-07-28 01:13:18 +0000

    deploy: a9d806ad1bfc90d74c4ae6101d1df38797f54702

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 //     activates it on demand. This guarantees deploys are picked up promptly
 //     (even on a resumed/backgrounded PWA) without yanking the bundle mid-task.
 
-const VERSION = 'prismos-ffda74c'
+const VERSION = 'prismos-a9d806a'
 const APP_SHELL = [
   '/',
   '/index.html',
